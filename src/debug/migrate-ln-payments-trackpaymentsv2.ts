411ac224111ed3a0733a415a2a5ce39200be3308
vindard <17693119+vindard@users.noreply.github.com>
2022-02-12 20:27:41 -0400

    chore: add tracing attributes/events to migrate function

--- src/debug/migrate-ln-payments-trackpaymentsv2.ts
+++ src/debug/migrate-ln-payments-trackpaymentsv2.ts
@@ -4,7 +4,12 @@ import { LedgerService } from "@services/ledger"
 import { LndService } from "@services/lnd"
 import { baseLogger } from "@services/logger"
 import { LnPaymentsRepository } from "@services/mongoose"
-import { asyncRunInSpan, SemanticAttributes } from "@services/tracing"
+import {
+  addAttributesToCurrentSpan,
+  addEventToCurrentSpan,
+  asyncRunInSpan,
+  SemanticAttributes,
+} from "@services/tracing"
 
 let lndService
 
@@ -18,21 +23,7 @@ export const migrateLnPaymentsFromLndByHash = async (): Promise<true> =>
 
       const paymentHashes = await LedgerService().listAllPaymentHashes()
       for await (const paymentHash of paymentHashes) {
-        const res = await asyncRunInSpan(
-          "debug.migrateLnPayment",
-          {
-            [SemanticAttributes.CODE_FUNCTION]: "debug.migrateLnPayment",
-          },
-          async (): Promise<true | LightningServiceError> => {
-            if (paymentHash instanceof Error) return paymentHash
-
-            const migrated = await migrateLnPayment(paymentHash)
-            if (migrated instanceof Error) return migrated
-
-            return true
-          },
-        )
-
+        const res = await migrateLnPayment(paymentHash)
         if (res instanceof Error) break
       }
       return true
@@ -40,57 +31,88 @@ export const migrateLnPaymentsFromLndByHash = async (): Promise<true> =>
   )
 
 const migrateLnPayment = async (
-  paymentHash: PaymentHash,
-): Promise<true | LightningServiceError> => {
-  // Check if hash exists in lnPayments repo
-  const lnPaymentsRepo = LnPaymentsRepository()
-  const lnPaymentExists = await lnPaymentsRepo.findByPaymentHash(paymentHash)
-  if (!(lnPaymentExists instanceof CouldNotFindError)) return true
+  paymentHash: PaymentHash | LightningServiceError,
+): Promise<true | LightningServiceError> =>
+  asyncRunInSpan(
+    "debug.migrateLnPayment",
+    {
+      [SemanticAttributes.CODE_FUNCTION]: "debug.migrateLnPayment",
+      "migrateLnPayment.paymentHash":
+        paymentHash instanceof Error ? paymentHash.name : paymentHash,
+    },
+    async (): Promise<true | LightningServiceError> => {
+      if (paymentHash instanceof Error) return paymentHash
 
-  // Fetch payment from lnd
-  const payment = await lndService.lookupPayment({
-    pubkey: lndService.defaultPubkey(),
-    paymentHash,
-  })
-  if (payment instanceof PaymentNotFoundError) return true
-  if (payment instanceof Error) return payment
+      // Check if hash exists in lnPayments repo
+      const lnPaymentsRepo = LnPaymentsRepository()
+      const lnPaymentExists = await lnPaymentsRepo.findByPaymentHash(paymentHash)
+      if (!(lnPaymentExists instanceof CouldNotFindError)) {
+        addAttributesToCurrentSpan({
+          "migrateLnPayment.skip": true,
+        })
+        addEventToCurrentSpan(`Payment already exists: ${paymentHash}`)
+        return true
+      }
 
-  // Add payment to LnPayments collection
-  const partialLnPayment = {
-    paymentHash,
-    paymentRequest: undefined,
-    sentFromPubkey: lndService.defaultPubkey(),
-    createdAt: undefined,
-  }
-  const newLnPayment =
-    payment.status === PaymentStatus.Pending
-      ? partialLnPayment
-      : payment.status === PaymentStatus.Failed
-      ? {
-          ...partialLnPayment,
-          status: PaymentStatus.Failed,
-          confirmedDetails: undefined,
-          attempts: undefined,
-          isCompleteRecord: true,
-        }
-      : {
-          ...partialLnPayment,
-          createdAt: payment.createdAt,
-          status: payment.status,
-          milliSatsAmount: payment.milliSatsAmount,
-          roundedUpAmount: payment.roundedUpAmount,
-          confirmedDetails: payment.confirmedDetails,
-          attempts: payment.attempts, // will be null, only comes from getPayments
-          isCompleteRecord: true,
-        }
+      // Fetch payment from lnd
+      const payment = await lndService.lookupPayment({
+        pubkey: lndService.defaultPubkey(),
+        paymentHash,
+      })
+      if (payment instanceof PaymentNotFoundError) {
+        addAttributesToCurrentSpan({ error: true })
+        addEventToCurrentSpan(
+          `Couldn't find hash ${paymentHash.substring(0, 5)}... in lnd`,
+        )
+        return true
+      }
+      if (payment instanceof Error) return payment
 
-  const updatedPaymentLookup = await LnPaymentsRepository().persistNew(newLnPayment)
-  if (updatedPaymentLookup instanceof Error) {
-    baseLogger.error(
-      { error: updatedPaymentLookup },
-      "Could not update LnPayments repository",
-    )
-  }
+      // Add payment to LnPayments collection
+      const partialLnPayment = {
+        paymentHash,
+        paymentRequest: undefined,
+        sentFromPubkey: lndService.defaultPubkey(),
+        createdAt: undefined,
+      }
+      const newLnPayment =
+        payment.status === PaymentStatus.Pending
+          ? partialLnPayment
+          : payment.status === PaymentStatus.Failed
+          ? {
+              ...partialLnPayment,
+              status: PaymentStatus.Failed,
+              confirmedDetails: undefined,
+              attempts: undefined,
+              isCompleteRecord: true,
+            }
+          : {
+              ...partialLnPayment,
+              createdAt: payment.createdAt,
+              status: payment.status,
+              milliSatsAmount: payment.milliSatsAmount,
+              roundedUpAmount: payment.roundedUpAmount,
+              confirmedDetails: payment.confirmedDetails,
+              attempts: payment.attempts, // will be null, only comes from getPayments
+              isCompleteRecord: true,
+            }
 
-  return true
-}
+      const updatedPaymentLookup = await LnPaymentsRepository().persistNew(newLnPayment)
+      if (updatedPaymentLookup instanceof Error) {
+        baseLogger.error(
+          { error: updatedPaymentLookup },
+          "Could not update LnPayments repository",
+        )
+        addAttributesToCurrentSpan({ error: true })
+        addEventToCurrentSpan(
+          `Could not update LnPayments repository (${updatedPaymentLookup.name})`,
+        )
+        return true
+      }
+
+      addAttributesToCurrentSpan({
+        "migrateLnPayment.success": `persistedNew: ${payment.paymentHash}`,
+      })
+      return true
+    },
+  )
